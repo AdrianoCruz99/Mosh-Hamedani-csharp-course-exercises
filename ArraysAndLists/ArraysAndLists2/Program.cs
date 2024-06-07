9a7946c2f06e95686a067a3a7380ebc7ecb15b4a
AdrianoCruz99 <adriano.two@outlook.com>
2024-06-07 19:41:03 -0300

    exercicio 2

---
+++ ArraysAndLists/ArraysAndLists2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ArraysAndLists2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to enter their name.Use an array to reverse the
+            //name and then store the result in a new string.Display the reversed name on the console.
+
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+
+            char[] nameArray = name.ToCharArray();
+            Array.Reverse(nameArray);
+
+            string reversedName = new string(nameArray);
+
+            Console.WriteLine(reversedName);
+        }
+    }
+}
