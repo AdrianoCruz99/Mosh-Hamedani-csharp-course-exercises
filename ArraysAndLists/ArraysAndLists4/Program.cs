f6f23d4c15cf646e1df57ff295752ee6c72e93e8
AdrianoCruz99 <adriano.two@outlook.com>
2024-06-08 15:25:31 -0300

    Exercicio 4

---
+++ ArraysAndLists/ArraysAndLists4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+namespace ArraysAndLists4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a program and ask the user to continuously enter a number or type "Quit" to exit.
+            //The list of numbers may include duplicates.
+            //Display the unique numbers that the user has entered.
+
+            
+            List<int> numbers = new List<int>();
+            while (true)
+            {
+                int number;
+                Console.WriteLine("Type a number or press any other key to exit: ");
+                string input = Console.ReadLine();
+                
+                if(int.TryParse(input, out number))
+                {
+                    if(!numbers.Contains(number))
+                    numbers.Add(number);
+                    
+                }
+                else
+                    break;
+            }
+            Console.WriteLine("Your numbers: ");
+            foreach(int number in numbers )
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
