00b7b46f92ae9f714747afde4925cc055de25eb7
AdrianoCruz99 <adriano.two@outlook.com>
2024-06-21 13:39:57 -0300

    classes

---
+++ ClassesExercises/ClassesExercises/ClassesExercises/Stopwatch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesExercises
+{
+    public class Stopwatch
+    {
+        private TimeSpan? _duration;
+        private DateTime? _startTime;
+        private DateTime? _stopTime;
+        private bool _running;
+
+        public void Start()
+        {
+            _running = true;
+            if (_running)
+            {
+                Console.WriteLine("Stopwatch is already running.");
+            }
+            _startTime = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            _stopTime = DateTime.Now;
+        }
+
+        public TimeSpan? TotalTime()
+        {
+            _duration = _stopTime.Value - _startTime.Value;
+            return _duration;
+        }
+
+    }
+}
---
+++ ClassesExercises/ClassesExercises/StackOverflowPost/Post.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowPost
+{
+    internal class Post
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        private DateTime _data { get; set; }
+
+        string key;
+        int up = 3;
+        int down = 7;   
+        public void Content(string title, string description)
+        {
+            Title = title;
+            Description = description;
+            Console.WriteLine(Title);
+            Console.WriteLine(Description);
+            _data = DateTime.Now;
+            Console.WriteLine(_data);
+            Console.WriteLine("--------------current votes---------------");
+            Console.WriteLine("up-votes:"+up+" down-votes:"+down);
+            Console.WriteLine();
+            Console.WriteLine("Press 'u' for up-vote or 'd' for down-vote");
+            string key = Console.ReadLine();
+            if (key == "u")
+            {
+                up++;
+            }
+            else if (key == "d")
+            {
+                down++;
+            }
+            Console.WriteLine("--------------current votes---------------");
+            Console.WriteLine("up-votes:" + up + "  down-votes:" + down);
+            
+
+
+        }
+       
+
+    }
+}
